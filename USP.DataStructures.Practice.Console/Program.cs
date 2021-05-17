110ede771b1dfdcfe95202505f6ac14f47af480c
Utkarsh Puranik <utkarsh.puranik@outlook.com>
2021-05-17 13:16:48 +0530

    Initial Draft

---
+++ USP.DataStructures.Practice.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace USP.DataStructures.Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ USP.DataStructures.Practice.Library/Queues.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace USP.DataStructures.Practice
+{
+    public class Queues
+    {
+    }
+}
